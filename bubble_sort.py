450a028d3c17aa7486bf711f6f50bd2be6e0a80d
Okello Gabriel Innocent <okellogabrielinnocent@gmail.com>
2021-04-15 18:04:15 +0300

    optimise swap

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,12 +4,16 @@ def bubble_sort(arr):
 	n=len(arr)
 
 	# Traverse through all array elements
-	for i in range(n-1):		
+	for i in range(n-1):
+		swapped = false		
 		# traverse the array from 0 to n-i-1
 		for j in range(0,n-i-1):
 			# Swap if the element found is greater than the next element
 			if arr[j]>arr[j+1]:
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
+			if swapped == false:
+				break
 
 
 arr = [64, 34, 25, 12, 22, 11, 90]
