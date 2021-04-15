4fc3aebf5fe8224d415bb6e7fd689f726d242051
Okello Gabriel Innocent <okellogabrielinnocent@gmail.com>
2021-04-15 22:51:24 +0300

    correct error on insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -12,7 +12,7 @@ def insertionSort(arr):
 
 	# Traverse through 1 to len(arr)
 	for i in range(1,len(arr):
-		key =arr[1]
+		key =arr[i]
 		# Move elements of arr[0..i-1], that are
         	# greater than key, to one position ahead
         	# of their current position
